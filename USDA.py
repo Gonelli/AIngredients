7bf503199fc15132c6d25b70ca17ef132dc70c5d
gonelli <gonelli@cs.utexas.edu>
2019-04-14 15:19:41 -0500

    cURL differentiation

--- USDA.py
+++ USDA.py
@@ -2,12 +2,11 @@ import shlex
 import subprocess
 
 class USDA:
+	# Tony's data.gov registered API key
+	API_key = "vy9T2DDU77D0ZBLadzrI0bJswyF2A6PXfrtG59D1"
 
 
-	def curl(nbdno, reportType):
-		# Tony's data.gov registered API key
-		API_key = "vy9T2DDU77D0ZBLadzrI0bJswyF2A6PXfrtG59D1"
-
+	def reportCurl(nbdno, reportType):
 		# Ugly command to minimize escapes
 		cmd = '''curl -H "Content-Type:application/json" -d '{'''
 		cmd += '''"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"]'''
@@ -16,6 +15,17 @@ class USDA:
 		args = shlex.split(cmd)
 		process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		stdout, stderr = process.communicate()
+		print(stdout)
+
+	def searchCurl(nbdno, reportType):
+		# # Ugly command to minimize escapes
+		# cmd = '''curl -H "Content-Type:application/json" -d '{'''
+		# cmd += '''"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"]'''
+		# cmd += ''',"type":"''' + reportType + '''"}' '''
+		# cmd += API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
+		# args = shlex.split(cmd)
+		# process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		# stdout, stderr = process.communicate()
 		# print(stdout)
 
 	def parseCurl(stdout):
@@ -26,4 +36,4 @@ class USDA:
 	nbdno = ["01009"]
 	# Report type: [b]asic or [f]ull or [s]tats
 	reportType = "b"
-	curl(nbdno, reportType)
+	reportCurl(nbdno, reportType)
