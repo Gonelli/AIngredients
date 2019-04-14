49941adcfe1d39877a8f1f007d8e9b5691214700
gonelli <gonelli@cs.utexas.edu>
2019-04-14 16:40:06 -0500

    Search implemented, json loading

--- USDA.py
+++ USDA.py
@@ -1,39 +1,39 @@
 import shlex
 import subprocess
+import json
 
-class USDA:
-	# Tony's data.gov registered API key
-	API_key = "vy9T2DDU77D0ZBLadzrI0bJswyF2A6PXfrtG59D1"
+# Tony's data.gov registered API key
+API_key = "vy9T2DDU77D0ZBLadzrI0bJswyF2A6PXfrtG59D1"
 
+def searchCurl(q):
+	cmd = '''curl -H "Content-Type:application/json" -d '{"q":"''' + q + '''","ds":"Standard Reference","max":"5","offset":"0"}' ''' + API_key + '''@api.nal.usda.gov/ndb/search'''
+	args = shlex.split(cmd)
+	process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+	return stdout
 
-	def reportCurl(nbdno, reportType):
-		# Ugly command to minimize escapes
-		cmd = '''curl -H "Content-Type:application/json" -d '{'''
-		cmd += '''"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"]'''
-		cmd += ''',"type":"''' + reportType + '''"}' '''
-		cmd += API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
-		args = shlex.split(cmd)
-		process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		stdout, stderr = process.communicate()
-		print(stdout)
+def getFoodID(q):
+	myJson = json.loads(searchCurl(q))
+	return myJson["list"]["item"][0]["ndbno"]
 
-	def searchCurl(nbdno, reportType):
-		# # Ugly command to minimize escapes
-		# cmd = '''curl -H "Content-Type:application/json" -d '{'''
-		# cmd += '''"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"]'''
-		# cmd += ''',"type":"''' + reportType + '''"}' '''
-		# cmd += API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
-		# args = shlex.split(cmd)
-		# process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		# stdout, stderr = process.communicate()
-		# print(stdout)
-
-	def parseCurl(stdout):
-		return stdout
+def reportCurl(nbdno, reportType):
+	cmd = '''curl -H "Content-Type:application/json" -d '{"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"],"type":"''' + reportType + '''"}' ''' + API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
+	args = shlex.split(cmd)
+	process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = process.communicate()
+	return stdout
 
 
-	# A list of up to 25 NDB numbers
-	nbdno = ["01009"]
-	# Report type: [b]asic or [f]ull or [s]tats
-	reportType = "b"
-	reportCurl(nbdno, reportType)
+# def parseCurl(stdout):
+# 	return stdout
+
+###############################################################################
+
+searchTerms = "salted butter"
+reportType = "b"
+reportJson = json.loads(reportCurl([getFoodID(searchTerms)], reportType))
+
+# Name of ingredient
+print(reportJson["foods"][0]["food"]["desc"]["name"])
+# Name of nutrient 0 (water)
+print(reportJson["foods"][0]["food"]["nutrients"][0]["name"])
