a394fdc6323962d5f23b30d4e08f0f784c150b8b
gonelli <gonelli@cs.utexas.edu>
2019-04-23 17:18:59 -0500

    Recipes.py added, not yet started

---
+++ Recipe List.py
@@ -0,0 +1,9 @@
+Macaroni and Cheese:
+	Dry Macaroni pasta			1 pound 
+	Salted butter				8 Tablespoons
+	Enriched all-purpose flour	4 Tablespoons
+	Table Salt 					1 Teaspoon
+	Reduced fat milk			2 cups 
+	Cheddar						2 cups 
+	Bread crumbs				1/2 cup 
+
---
+++ Recipes.py
@@ -0,0 +1,8 @@
+from USDA import Ingredient
+
+class Recipe:
+	vegetarian = False
+	ingredients = []
+
+	def __init__(self, ingredientList):
+		print("Hello World!")
