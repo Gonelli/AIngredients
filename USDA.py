102f25070842e6f8879565fb0f4f064c0d1ad208
gonelli <gonelli@cs.utexas.edu>
2019-04-23 15:59:34 -0500

    Ingredient class set-up

--- USDA.py
+++ USDA.py
@@ -5,35 +5,47 @@ import json
 # Tony's data.gov registered API key
 API_key = "vy9T2DDU77D0ZBLadzrI0bJswyF2A6PXfrtG59D1"
 
-def searchCurl(q):
-	cmd = '''curl -H "Content-Type:application/json" -d '{"q":"''' + q + '''","ds":"Standard Reference","max":"5","offset":"0"}' ''' + API_key + '''@api.nal.usda.gov/ndb/search'''
-	args = shlex.split(cmd)
-	process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	stdout, stderr = process.communicate()
-	return stdout
+class Ingredient:
+	reportJson = None
+	name = ""
+	nutrients = {} # Nutrient name: (Amount per 100g of ingredient, Unit)
+	nutrientsDetailed = [] # Fully detailed nutient list with measurement options
 
-def getFoodID(q):
-	myJson = json.loads(searchCurl(q))
-	return myJson["list"]["item"][0]["ndbno"]
+	def __init__(self, searchTerms):
+		# Defaults to [b]asic report stats
+		self.reportJson = json.loads(self.getReport([self.getFoodID(searchTerms)], "b"))
+		self.name = self.reportJson["foods"][0]["food"]["desc"]["name"]
+		self.nutrient0 = self.reportJson["foods"][0]["food"]["nutrients"][0]["name"]
 
-def reportCurl(nbdno, reportType):
-	cmd = '''curl -H "Content-Type:application/json" -d '{"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"],"type":"''' + reportType + '''"}' ''' + API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
-	args = shlex.split(cmd)
-	process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	stdout, stderr = process.communicate()
-	return stdout
+		# Add all nutrient lists to nutrients[]
+		for nutrient in self.reportJson["foods"][0]["food"]["nutrients"]:
+			self.nutrientsDetailed.append(nutrient)
+			self.nutrients[nutrient["name"]] = (nutrient["value"], nutrient["unit"])
 
+	"""
+	Takes an input string (q) and searches USDA's database to get a list of possible matching non-branded food items. Returns the nbdno of the top result.
+	"""
+	def getFoodID(self, q):
+		cmd = '''curl -H "Content-Type:application/json" -d '{"q":"''' + q + '''","ds":"Standard Reference","max":"5","offset":"0"}' ''' + API_key + '''@api.nal.usda.gov/ndb/search'''
+		args = shlex.split(cmd)
+		process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		myJson = json.loads(stdout)
+		# for item in myJson["list"]["item"]:
+		# 	print(item["name"])
+		return myJson["list"]["item"][0]["ndbno"]
+	
+	"""
+	Takes an food ID (nbdno) and a report type character ([b]asic, [f]ull, or [s]tats), then returns a json file of the food's nutritional data. 
+	"""
+	def getReport(self, nbdno, reportType):
+		cmd = '''curl -H "Content-Type:application/json" -d '{"ndbno":["''' + "\",\"".join([str(x) for x in nbdno]) + '''"],"type":"''' + reportType + '''"}' ''' + API_key + '''@api.nal.usda.gov/ndb/V2/reports'''
+		args = shlex.split(cmd)
+		process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		stdout, stderr = process.communicate()
+		return stdout
 
-# def parseCurl(stdout):
-# 	return stdout
 
 ###############################################################################
-
-searchTerms = "salted butter"
-reportType = "b"
-reportJson = json.loads(reportCurl([getFoodID(searchTerms)], reportType))
-
-# Name of ingredient
-print(reportJson["foods"][0]["food"]["desc"]["name"])
-# Name of nutrient 0 (water)
-print(reportJson["foods"][0]["food"]["nutrients"][0]["name"])
+myIngredient = Ingredient("Salted Butter")
+print(myIngredient.name)
